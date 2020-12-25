4987425f2d6a81ba217f0eb020293c284f7199d7
Greyson Rampazo <greysonf@gmail.com>
2020-12-25 00:26:18 -0300

    project commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    loadRepositories()
+  }, [repositories])
+
+  async function loadRepositories() {
+    const { data } = await api.get('/repositories')
+
+    setRepositories(data)
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      title: "Meu Repositorio",
+      url: "https://github.com/greysonrzf/gochallenge_repo_backend",
+      techs: ["NodeJs", "JavaScript"]
+    }
+
+    loadRepositories()
+    try {
+      await api.post('/repositories', data)
+    } catch (err) {
+      console.log(err)
+    }
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+
+    loadRepositories()
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <div className="repositoryContainer">
+              <p className="title">{repository.title}</p>
+              <p className="text">{repository.techs.join(', ')}</p>
+              <p className="text">{repository.url}</p>
+            </div>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
